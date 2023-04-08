beb1b9b76b5c55aa4dc05caafeba17af60dacc4c
Irsyad R <irsyad711@gmail.com>
2023-04-08 17:35:19 +0700

    Initial Commit

---
+++ PayTax/PayTaxApp.swift
@@ -0,0 +1,17 @@
+//
+//  PayTaxApp.swift
+//  PayTax
+//
+//  Created by M Irsyad R on 08/04/23.
+//
+
+import SwiftUI
+
+@main
+struct PayTaxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
